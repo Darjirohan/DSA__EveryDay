4b04dc3727c4687af844ec7345a545f649eee3f0
Rohan Darji <rohan672006@gmail.com>
2026-03-24 22:15:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,19 +1,25 @@
 class Solution {
-    public boolean isPrime(int num){
-            for(int j=2;j<=Math.sqrt(num);j++){
-            if(num%j==0){
-                return false;
-            }  
-            }
-        return true;
-    }
     public int countPrimes(int n) {
-    int count=0;
-    for(int i=2;i<n;i++){
-        if(isPrime(i)){
-            count++;
+        if (n <= 2) return 0;
+
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
         }
-    }
-    return count;  
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) count++;
+        }
+
+        return count;
     }
 }
