03f7ccdc554248ff7e849eefbe26d5f429d1aca5
Rohan Darji <rohan672006@gmail.com>
2026-03-30 10:43:12 +0530

    Time: 74 ms (72.97%), Space: 117 MB (66.67%) - LeetHub

--- 3781-maximum-score-after-binary-swaps/3781-maximum-score-after-binary-swaps.java
+++ 3781-maximum-score-after-binary-swaps/3781-maximum-score-after-binary-swaps.java
@@ -1,6 +1,6 @@
 class Solution {
     public long maximumScore(int[] nums, String s) {
-    PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+    PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b - a);
     long ans=0;
     for(int i=0;i<s.length();i++){
         pq.offer(nums[i]);
