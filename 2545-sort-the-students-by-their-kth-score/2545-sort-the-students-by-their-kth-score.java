13826cb127c6b4d6dbf04d6de8c523677bbaf489
Rohan Darji <rohan672006@gmail.com>
2026-03-31 21:17:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.java
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.java
@@ -1,28 +1,7 @@
 class Solution {
     public int[][] sortTheStudents(int[][] score, int k) {
         
-        int m = score.length;
-        int n = score[0].length;
-
-        int[][] mat = new int[m][n];
-        Integer[] arr = new Integer[m];
-        HashMap<Integer, Integer> mp = new HashMap<>();
-
-        for (int i = 0; i < m; i++) {
-            arr[i] = score[i][k];
-            mp.put(arr[i], i);
-        }
-
-        Arrays.sort(arr, (a, b) -> b - a);
-
-        for (int i = 0; i < m; i++) {
-            int index = mp.get(arr[i]); 
-
-            for (int j = 0; j < n; j++) {
-                mat[i][j] = score[index][j];
-            }
-        }
-
-        return mat;
+        Arrays.sort(score,(a,b)->b[k]-a[k]);
+        return score;
     }
 }
