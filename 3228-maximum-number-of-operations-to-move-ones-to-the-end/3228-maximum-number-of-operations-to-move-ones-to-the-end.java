10b0a34fd6a873603ac4f49ce7b27a3b6de3f19f
Rohan Darji <rohan672006@gmail.com>
2026-05-04 13:33:26 +0530

    Time: 9 ms (11.04%), Space: 47.3 MB (81.68%) - LeetHub

---
+++ 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxOperations(String s) {
+       int count = 0;
+       int ans = 0;
+
+       for(int i=0;i<s.length();i++){
+        if(s.charAt(i)=='1') count++;
+        if(i>0 && (s.charAt(i)=='0'&&s.charAt(i-1)=='1')) ans+=count;
+       } 
+       return ans;
+    }
+}
