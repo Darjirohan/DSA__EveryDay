9d8aac47b12aae2c7a33432496bb406916323a38
Rohan Darji <rohan672006@gmail.com>
2026-05-23 22:22:53 +0530

    Time: 1 ms (99.48%), Space: 42.4 MB (69.04%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isUgly(int n) {
+    boolean isUgly = false;
+    if(n==0){
+    isUgly = false;
+    }else{
+    while(n%2==0){
+        n/=2;
+    }
+    while(n%3==0){
+        n/=3;
+    }
+    while(n%5==0){
+        n/=5;
+    }
+    if(n==1){
+        isUgly=true;
+    }
+    }
+    return isUgly;
+    }
+}
